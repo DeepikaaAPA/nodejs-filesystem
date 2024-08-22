12ffba18f083dee33956d1525d1316c741564d9d
DeepikaaAPA <deepikaarjun@gmail.com>
2024-08-22 09:43:25 +0530

    sending one hardcoded file

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-const fs = require("fs");
-
 // import the express module
 const express = require("express");
-
+const fs = require("fs");
 // create an express application
 const app = express();
-
-// use the express middleware for parsing json data
-app.use(express.json());
-
-// Define a route handler for the default "GET" request "/"
-app.get("/createFile", (request, response) => {
-  // your logic goes here to get the current timestamp and format it
-  // and make it as a file name and the value to be written in the file
+const path = require("path");
+const getAllFiles = (request, response) => {
+  response.sendFile(path.join(__dirname, "/files/8-18-2024, 12-03-26 AM.txt"));
+};
+const createFile = (request, response) => {
+  //  logic goes here to get the current timestamp and format it
+  // and make it as a file name and to be written in the file
   let current_date = new Date();
   let file_name = current_date
     .toLocaleString()
@@ -24,43 +21,15 @@ app.get("/createFile", (request, response) => {
     response.send(`${file_name}.txt created successfully`);
     console.log(`files/${file_name}.txt created successfully`);
   });
-});
-
-app.get("/getAllFiles", (request, response) => {
-  const data = [];
-
-  const directoryPath = "./files";
-
-  // Use fs.readdir to read the contents of the directory asynchronously
-  fs.readdir(directoryPath, (err, files) => {
-    if (err) {
-      console.error("Error reading directory:", err);
-      response.send("Error reading directory");
-      return;
-    }
-
-    console.log("Files and folders in the directory:", files);
-    let fileContent = "";
-    files.forEach((file) => {
-      try {
-        fileContent = fs.readFileSync(directoryPath + "\\" + file, "utf-8");
-        console.log(`Contents of ${file}:`, fileContent);
-        data.push({
-          name: file,
-          content: new Date(fileContent),
-        });
-      } catch (err) {
-        console.error(`Error reading ${file}:`, err);
-      }
-    });
-    // Sorting the array based on the 'date' property
-    data.sort((a, b) => a.content - b.content);
-    console.log(data);
-    response.json(data);
-  });
-});
-
-// start the server and listen on port 3001
-app.listen(3001, () => {
-  console.log(`Server running on port 3001 at http://127.0.0.1:3001`);
+};
+app.use(express.static("files"));
+// use the express middleware for parsing json data
+app.use(express.json());
+// Define a route handler for the  "GET" request "/createFile"
+app.get("/createFile", createFile);
+// Define a route handler for the  "GET" request "/getAllFiles"
+app.get("/getAllFiles", getAllFiles);
+// start the server and listen on port 3000
+app.listen(3000, () => {
+  console.log(`Server running on port 3000 at http://127.0.0.1:3000`);
 });
